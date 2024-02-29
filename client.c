cb56be02c0a76cde354b31960a003d5ef361f6b0
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-29 19:34:43 +0300

    Update client.c
    created the client

---
+++ client.c
@@ -0,0 +1,10 @@
+#include <strings.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+ 
+int main(int argc, char*  
